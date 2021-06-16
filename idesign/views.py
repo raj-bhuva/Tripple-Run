9f7153a78b2d75d941b5cd3d6a0420b3f92fcc8e
raj-bhuva <69042209+raj-bhuva@users.noreply.github.com>
2021-06-16 21:03:48 +0530

    Update views.py

--- idesign/views.py
+++ idesign/views.py
@@ -230,11 +230,12 @@ def login(request):
         username = request.POST['username']
         password = request.POST['password']
         try:
-            user = auth.authenticate(username=User.objects.get(
-                email=username), password=password)
-        except:
             user = auth.authenticate(username=username, password=password)
 
+           
+        except:
+            user = auth.authenticate(username=User.objects.get(
+                email=username), password=password)
         if user is not None:
             if user.is_active:
                 auth.login(request, user)
