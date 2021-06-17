a268a63e64585a5b7bfe2a6d1c26ab8a5e87d64f
raj-bhuva <69042209+raj-bhuva@users.noreply.github.com>
2021-06-17 17:48:06 +0530

    Update settings.py

--- smoak/settings.py
+++ smoak/settings.py
@@ -167,8 +167,8 @@ USE_TZ = True
 
 STATIC_URL = 'https://tripple-run.s3.amazonaws.com/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR,'static')
 
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
