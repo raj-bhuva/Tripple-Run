09e1384ef70a8b2c1ca43e95f3b1a2b69cdc9a7b
raj-bhuva <69042209+raj-bhuva@users.noreply.github.com>
2021-06-17 17:31:55 +0530

    Update settings.py

--- smoak/settings.py
+++ smoak/settings.py
@@ -165,7 +165,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'https://tripple-run.s3.amazonaws.com/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
@@ -177,7 +177,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'https://tripple-run.s3.amazonaws.com/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
