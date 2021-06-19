eaa513b65910f446fb3512afbdcb0007ce47ec24
raj-bhuva <69042209+raj-bhuva@users.noreply.github.com>
2021-06-19 15:40:59 +0530

    Update views.py

--- idesign/views.py
+++ idesign/views.py
@@ -118,7 +118,7 @@ def singal_product(request, id):
     ZipFile = zipfile.ZipFile("./"+filename, "w")
     for a in allfile:
         print('zip111111 = ',STATIC_URL+a.file.name)
-        ZipFile.write(STATIC_URL+a.file.name, os.path.basename(STATIC_URL+a.file.name),
+        ZipFile.write('https://tripple-run.s3.us-east-2.amazonaws.com/S101.zip', os.path.basename('https://tripple-run.s3.us-east-2.amazonaws.com/S101.zip'),
                       compress_type=zipfile.ZIP_DEFLATED)
 #       ZipFile.write(a.file.path, os.path.relpath(a.file.path, './media/pics/Product_file'),
 #                       compress_type=zipfile.ZIP_DEFLATED)
