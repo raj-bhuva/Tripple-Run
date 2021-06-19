832bfac9d9d851dae8bb1cbe130e6cc54072a94e
raj-bhuva <69042209+raj-bhuva@users.noreply.github.com>
2021-06-19 14:39:52 +0530

    Update views.py

--- idesign/views.py
+++ idesign/views.py
@@ -118,7 +118,8 @@ def singal_product(request, id):
     filename = context["data"].design_code+'.zip'
     ZipFile = zipfile.ZipFile("./"+filename, "w")
     for a in allfile:
-        ZipFile.write(a.file.name, os.path.relpath(a.file.name, '../'),
+        print('zip111111 = ', a.file.name)
+        ZipFile.write(a.file.name, os.path.relpath(a.file.name, 'media/pics/Product_file'),
                       compress_type=zipfile.ZIP_DEFLATED)
 #       ZipFile.write(a.file.path, os.path.relpath(a.file.path, './media/pics/Product_file'),
 #                       compress_type=zipfile.ZIP_DEFLATED)
