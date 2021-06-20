ee4770d00deaf666706ad8185b88bce221720f22
raj-bhuva <69042209+raj-bhuva@users.noreply.github.com>
2021-06-20 05:33:51 +0530

    Update views.py

--- idesign/views.py
+++ idesign/views.py
@@ -173,8 +173,10 @@ def singal_product(request, id):
     zip_buffer = io.BytesIO()
     with zipfile.ZipFile(zip_buffer, "a", zipfile.ZIP_DEFLATED, False) as zipper:
         for a in allfile:
-            infile_object = s3.get_object(Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key = a.file.name) 
+            infile_object = s3.get_object(Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key = a.file.name)
+            print('aaaaaaaaaaooo=',infile_object)
             infile_content = infile_object['Body'].read()
+            print('aaaaaaaaaafff=',infile_content)
         zipper.writestr(filename, infile_content)
 
     s3.put_object(Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key='zipfiles/'+filename, Body=zip_buffer.getvalue())
