9f58cbd80ccf0b844e21f2dd83308f00abe09f05
raj-bhuva <69042209+raj-bhuva@users.noreply.github.com>
2021-06-21 10:11:05 +0530

    Update views.py

--- idesign/views.py
+++ idesign/views.py
@@ -168,25 +168,25 @@ def singal_product(request, id):
 #     print(a.file.path)
 #     resp = HttpResponse(byte.getvalue(), content_type="application/x-zip-compressed")
 #     resp['Content-Disposition'] = 'attachment; filename=%s' % zip_filename
-    abc = []
-    for a in allfile:
-        abc.append(a.file.name)
+    
+   
+        
     zip_buffer = io.BytesIO()
     
     with zipfile.ZipFile(zip_buffer, "a", zipfile.ZIP_DEFLATED, False) as zipper:
-        
-        print('a.file = = ', a.file.name)
-        print('a.file.url = = ', a.file.url)
-       
+        for a in allfile:
+            print('a.file = = ', a.file.name)
+            print('a.file.url = = ', a.file.url)
 
-        infile_object = s3.get_object(Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key = abc[0])
-#             print('aaaaaaaaaaooo=',infile_object)
 
-        infile_content = infile_object['Body'].read()
-        
+            infile_object = s3.get_object(Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key = a.file.name)
+    #             print('aaaaaaaaaaooo=',infile_object)
 
-#             print('aaaaaaaaaafff=',infile_content)
-        zipper.writestr(abc[0], infile_content)
+            infile_content = infile_object['Body'].read()
+
+
+    #             print('aaaaaaaaaafff=',infile_content)
+            zipper.writestr(a.file.name, infile_content)
 
     s3.put_object(Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key='zipfiles/'+filename, Body=zip_buffer.getvalue())
     # get category tag
