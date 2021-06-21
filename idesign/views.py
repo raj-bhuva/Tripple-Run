6934a99af18f096a94eadd8f70f8540d3616e914
raj-bhuva <69042209+raj-bhuva@users.noreply.github.com>
2021-06-21 11:16:30 +0530

    Update views.py

--- idesign/views.py
+++ idesign/views.py
@@ -175,9 +175,10 @@ def singal_product(request, id):
     
     with zipfile.ZipFile(zip_buffer, "a", zipfile.ZIP_DEFLATED, False) as zipper:
         for a in allfile:
+            print('adadvad',a.file)
             infile_object = s3.get_object(Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key = a.file.name)
             infile_content = infile_object['Body'].read()
-            zipper.writestr("./", infile_content)
+            zipper.writestr(a.file.name, infile_content)
 
     s3.put_object(Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key='zipfiles/'+filename, Body=zip_buffer.getvalue())
     # get category tag
