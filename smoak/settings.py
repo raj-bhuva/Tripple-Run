81717007abf8efb695790e45bf1ec6ef48eae5d6
raj-bhuva <69042209+raj-bhuva@users.noreply.github.com>
2021-07-30 23:04:18 +0530

    added django-heroku database

--- smoak/settings.py
+++ smoak/settings.py
@@ -14,7 +14,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from decouple import config
 import os
 from pathlib import Path
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -114,17 +114,18 @@ WSGI_APPLICATION = 'smoak.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('NAME'),
-        'USER': config('USER'),
-        'PASSWORD':config('PASSWORD'),
-        'HOST': config('HOST'),
-        'PORT': config('PORT')
-    }
-}
+# -------------------------------------------------------------------------
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config('NAME'),
+#         'USER': config('USER'),
+#         'PASSWORD':config('PASSWORD'),
+#         'HOST': config('HOST'),
+#         'PORT': config('PORT')
+#     }
+# }
+# -------------------------------------------------------------------------
 import dj_database_url
 
 DATABASES['default'] = dj_database_url.config(default=os.environ["DATABASE_URL"])
@@ -164,22 +165,38 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+# -------------------------------------------------------------------------
+# STATIC_URL = 'https://tripple-run.s3.amazonaws.com/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
-STATIC_URL = 'https://tripple-run.s3.amazonaws.com/static/'
+# MEDIA_URL = 'https://tripple-run.s3.amazonaws.com/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# -------------------------------------------------------------------------
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+django_heroku.settings(locals())
+
+
+
+
+
 
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR,'static')
 
 # ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-
-
-MEDIA_URL = 'https://tripple-run.s3.amazonaws.com/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 
 # APPEND_SLASH = False
 
@@ -220,21 +237,22 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 # cart
 CART_SESSION_ID = 'cart'
 
-
+# ---------------------------------------------------------
 #S3 BUCKETS CONFIG
 
-AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+# AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+# AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+# AWS_S3_FILE_OVERWRITE = False
+# AWS_DEFAULT_ACL = None
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-AWS_S3_REGION_NAME = 'us-east-2' #change to your region
-AWS_S3_SIGNATURE_VERSION = 's3v4'
-AWS_S3_ADDRESSING_STYLE = "virtual"
-AWS_DEFAULT_ACL = 'private'
+# AWS_S3_REGION_NAME = 'us-east-2' #change to your region
+# AWS_S3_SIGNATURE_VERSION = 's3v4'
+# AWS_S3_ADDRESSING_STYLE = "virtual"
+# AWS_DEFAULT_ACL = 'private'
+# --------------------------------------------------------------------
 # AWS_QUERYSTRING_AUTH = False
 
 # AWS_QUERYSTRING_AUTH = False
